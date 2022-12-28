6a8783a1f0375240c9d3a2ad164079adebd9ca1f
sw-phuth <69314040+thanhvinh941@users.noreply.github.com>
2022-12-28 13:06:37 +0700

    create MemberRegisRequestDto

---
+++ Member-Service/src/main/java/com/se1/memberservice/domain/request/dto/MemberRegisRequestDto.java
@@ -0,0 +1,26 @@
+package com.se1.memberservice.domain.request.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MemberRegisRequestDto {
+
+	private String memberFName;
+	
+	private String memberLName;
+	
+	private String memberEmail;
+	
+	private String memberPhoneNumber;
+	
+	private String displayName;
+	
+	private String loginId;
+	
+	private String password;
+	
+	private Date dboDt;
+	
+}
