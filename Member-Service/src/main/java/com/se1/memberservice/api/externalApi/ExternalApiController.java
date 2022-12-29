5f3914ea425a05d53c44bf7eacbabb571597cdf5
sw-phuth <69314040+thanhvinh941@users.noreply.github.com>
2022-12-29 17:56:42 +0700

    Update ExternalApiController.java

--- Member-Service/src/main/java/com/se1/memberservice/api/externalApi/ExternalApiController.java
+++ Member-Service/src/main/java/com/se1/memberservice/api/externalApi/ExternalApiController.java
@@ -1,6 +1,7 @@
 package com.se1.memberservice.api.externalApi;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,21 @@ public class ExternalApiController {
 	
 	@PostMapping("/existMemberLoginId")
 	public ResponseEntity<?> existMemberLoginId(){
+		return null;
+	}
+	
+	@PostMapping("/updateMemberInfo")
+	public ResponseEntity<?> updateMemberInfo(){
+		return null;
+	}
+	
+	@GetMapping("/forgetPassword")
+	public ResponseEntity<?> forgetPassword(){
+		return null;
+	}
+	
+	@GetMapping("/resetPassword")
+	public ResponseEntity<?> resetPassword(){
 		return null;
 	}
 	
