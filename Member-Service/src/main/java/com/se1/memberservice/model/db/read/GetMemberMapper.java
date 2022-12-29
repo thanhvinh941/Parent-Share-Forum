9e09f3281f222cc5bea0477f9c4b986e3babc998
sw-phuth <69314040+thanhvinh941@users.noreply.github.com>
2022-12-29 13:32:34 +0700

    mapper user

--- Member-Service/src/main/java/com/se1/memberservice/model/db/read/GetMemberMapper.java
+++ Member-Service/src/main/java/com/se1/memberservice/model/db/read/GetMemberMapper.java
@@ -1,8 +1,15 @@
 package com.se1.memberservice.model.db.read;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.se1.memberservice.domain.entity.Member;
 
 @Mapper
 public interface GetMemberMapper {
 
+	
+	List<Member> selectWhere(@Param("where") String where);
 }
--- Member-Service/src/test/java/com/se1/memberservice/MemberServiceApplicationTests.java
+++ Member-Service/src/test/java/com/se1/memberservice/MemberServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.se1.memberservice;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.se1.memberservice.domain.entity.Member;
+import com.se1.memberservice.model.db.read.GetMemberMapper;
 
 @SpringBootTest
 class MemberServiceApplicationTests {
 
+	@Autowired
+	GetMemberMapper mapper;
+	
 	@Test
 	void contextLoads() {
+		List<Member> list = mapper.selectWhere(null);
+		System.out.println(list);
 	}
 
 }
